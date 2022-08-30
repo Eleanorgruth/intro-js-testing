85c5c1b7bf662728a1bac5395833ecc0a5ceafe8
Eleanor Ruth <ellie.ruth@me.com>
2022-08-30 15:25:24 -0600

    Complete class

--- tweet.js
+++ tweet.js
@@ -1,5 +1,29 @@
 class Tweet {
+  constructor(sender, content, link) {
+    this.sender = sender;
+    this.content = content;
+    this.link = link || 'no link provided';
+    // this does the same thing as the conditional
+        // if (this.link === undefined) {
+        //   this.link = 'no link provided'
+        // }
+    this.likes = 0;
+    this.retweets = 0;
+    this.replies = [];
+    this.replyCount = 0;
+  }
+  like() {
+    this.likes ++
+  }
+  retweet() {
+    this.retweets ++
+  }
+  reply(replyText) {
+    this.replies.push(replyText)
+    this.replyCount ++
+  }
 
 }
+
 
 module.exports = Tweet;
